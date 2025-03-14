6779e1c1b3e7bf48e6edd1277f194b263eb62c67
LeeCh0129 <cksgh5477@gmail.com>
2025-03-14 17:19:24 +0900

    feat: 로그인 페이지에 토스트 메시지 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,16 +3,28 @@
 import { pretendard } from '@/styles/fonts';
 import './globals.css';
 import Providers from './providers';
+import { useEffect, useState } from 'react';
+import { RecoilRoot } from 'recoil';
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  // 클라이언트 사이드 렌더링을 위한 상태 추가
+  const [mounted, setMounted] = useState(false);
+
+  // 컴포넌트가 마운트된 후에만 렌더링
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   return (
     <html lang="ko" className={pretendard.variable}>
       <body>
-        <Providers>{children}</Providers>
+        <RecoilRoot>
+          {!mounted ? children : <Providers>{children}</Providers>}
+        </RecoilRoot>
       </body>
     </html>
   );
